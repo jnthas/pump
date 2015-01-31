3a432d0ff9c92c749a70b6d2c75e33e9f136b444
Jonathas Amaral <jnthas@gmail.com>
2015-01-31 00:43:34 -0200

    Modificado bibliotecas do projeto, inicio do app sem CSS

--- app/js/app.js
+++ app/js/app.js
@@ -1,37 +1,30 @@
-$(function(){
+(function(){
 
-  var Aluno = Backbone.Model.extend({
-    nome: "",
-    nascimento: 0,
-    peso: 0,
-    altura: 0,
-    sexo: "m"
-  });
-  
-  var AlunoView = Backbone.View.extend({
-    el: $("#hello"),
-    template: _.template($('#hello-template').html()),
-    
-    initialize: function() {
-      this.render();
-    },
-    
-    render: function() {
-      this.$el.html(this.template({text: "Hello, World!"}));
-      return this;
-    }
-  
-  });
-  
-  var AppView = Backbone.View.extend({
+  var app = angular.module("pump", []);
 
-    initialize: function() {
-      var view = new AlunoView();
-    }
+
+  app.controller("StudentController", function() {
+
+    this.model = student;
+
+    this.save = function(m) {
+      console.log("Saving... " + this.model.name);
+      this.model = m;
+    };
 
   });
 
-  var App = new AppView();
 
 
-});
+
+
+  var student = {
+    name: "Jonathas Amaral",
+    born: "02/05/1985",
+    weight: 65,
+    height: 174
+  };
+
+
+
+})();
