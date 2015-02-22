c70c7b0766193b6573dacfc311931c9f662ea272
Jonathas Amaral <jnthas@gmail.com>
2015-02-22 00:04:40 -0300

    Iniciado criaçao de testes unitarios com jasmin.

---
+++ test/unit/dataServiceSpec.js
@@ -0,0 +1,26 @@
+describe("Student Data Service Test Suit", function() {
+  
+  it("should have a blank stundent object at startup", function() {      
+  });
+  
+  it("should record a student", function() {      
+  });
+    
+  it("should update the student record", function() {      
+  });
+  
+  it("should delete the student record", function() {      
+  });  
+
+});
+
+
+
+describe("Training Plan Data Service Test Suit", function() {
+  
+});
+
+
+describe("Exercises Data Service Test Suit", function() {
+  
+});
